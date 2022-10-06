c7a3c9a9d0a023864070e137b27bde9ec13b6c5c
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-10-06 19:02:23 -0400

    Created and Styled Header Component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,10 @@
+import igniteLogo from "../../assets/Ignite-logo.svg";
 import styles from "./styles.module.css";
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="ignite logo" title="ignite logo" />
     </header>
   );
 }
