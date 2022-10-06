d9df95499712d313b1c754bff983fd0bec01d829
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-10-06 18:42:41 -0400

    Added Styles Global and Fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
+import { Header } from "./components/Header";
+import "./styles/global.css";
+
 function App() {
-  return <h1>Hello Ignite Feed</h1>;
+  return <Header />;
 }
 
 export default App;
