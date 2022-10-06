6c925a0bce7c1cc0df4bb9b9a75f3a377d4a682b
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-10-06 19:13:31 -0400

    Styled App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { Header } from "./components/Header";
+import styles from "./App.module.css";
 import "./styles/global.css";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main className={styles.container}>
+        <aside>sidebar</aside>
+        <section></section>
+      </main>
+    </>
+  );
 }
 
 export default App;
